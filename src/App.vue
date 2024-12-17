e2dd6ee476d4b1ae058fbb9fe7ebc60f16d2e24f
test <test@MacBook-Pro-Malik.local>
2024-12-17 07:53:23 +0500

    add new structure

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,59 @@
 import {reactive} from 'vue'
 const form = reactive({
   name: null,
+  email: null,
+  massage: null,
+  password: null,
+  currentPassword: null,
+  checkbox: null
 })
 </script>
 
 <template>
   <div class="container">
     <div class="fields">
-      <label class="name">Имя</label>
+      <label class="label">Имя</label>
     </div>
     <div class="control">
-      <input v-model="form.name" type="text" placeholder="Введите имя" />
+      <input class="input" v-model="form.name" type="text" placeholder="Введите имя" />
     </div>
-    {{ form.name }}
+    <div class="fields">
+      <label class="label">Почта</label>
+    </div>
+    <div class="control">
+      <input class="input" v-model="form.email" type="text" placeholder="Введите email" />
+    </div>
+    <div class="fields">
+      <label class="label">Сообщение</label>
+    </div>
+    <div class="control">
+      <textarea class="textarea" v-model="form.massage" type="text" placeholder="Введите сообщение" />
+    </div>
+    <div class="fields">
+      <label class="label">Пароль</label>
+    </div>
+    <div class="control">
+      <input class="input" v-model="form.password" type="password" placeholder="Введите пароль" />
+    </div>
+    <div class="fields">
+      <label class="label">Повторите пароль</label>
+    </div>
+    <div class="control">
+      <input class="input" v-model="form.currentPassword" type="text" placeholder="Повторите пароль" />
+    </div>
+    <div class="field">
+      <div class="control">
+        <label class="label">
+          <input v-model="form.checkbox" type="checkbox">
+          я соглашаюсь<a href="#">правилами сообщества</a>
+        </label>
+      </div>
+    </div>
+    <div class="control mt-4 mx-auto">
+      <button class="button is-link" @click="onSubmit">
+        Отправить
+      </button>
+    </div>
   </div>
 </template>
 
